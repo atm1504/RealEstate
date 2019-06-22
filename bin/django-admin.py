8d105a481c6309fbc8399cd24a035ac282be3caf
Amartya Mondal <hayyoulistentome@gmail.com>
2019-06-22 13:01:50 +0530

    started project

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/amartyamondal/Documents/Django/btre_project/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python3.6/sre_constants.py
@@ -0,0 +1,1 @@
+/Users/amartyamondal/anaconda3/lib/python3.6/sre_constants.py
